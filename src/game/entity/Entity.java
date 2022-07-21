ce931339af27e7b0ba746f375b4ccf6928463f83
Original-Developer <morenoruiz09042003@gmail.com>
2022-07-21 14:21:11 +0200

    Updated Entity.java

--- src/game/entity/Entity.java
+++ src/game/entity/Entity.java
@@ -1,6 +1,5 @@
 package game.entity;
 
-<<<<<<< HEAD
 import java.awt.image.BufferedImage;
 
 public class Entity {
@@ -10,9 +9,4 @@ public class Entity {
     public BufferedImage walk_left1, walk_right1, walk_up1, walk_down1;
     public BufferedImage walk_left2, walk_right2, walk_up2, walk_down2;
     public byte dir;
-=======
-public class Entity {
-    public int x, y;
-    public byte speed;
->>>>>>> origin/main
 }
