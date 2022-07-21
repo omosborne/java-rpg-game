db018dbab2888be70fb5b887151b2e6ed17577bc
Original-Developer <morenoruiz09042003@gmail.com>
2022-07-21 14:39:22 +0200

    Set up Main Char imgs

--- src/game/entity/Player.java
+++ src/game/entity/Player.java
@@ -24,7 +24,23 @@ public class Player extends Entity {
 
     public void getPlayerImage () {
         try {
-            idle_up = ImageIO.read(getClass().getResourceAsStream("../images/idle_up.png"));
+            // Idle Sprites
+            idle_up = ImageIO.read(getClass().getResourceAsStream("/game/images/character/up_idle.png"));
+            idle_down = ImageIO.read(getClass().getResourceAsStream("/game/images/character/down_idle.png"));
+            idle_left = ImageIO.read(getClass().getResourceAsStream("/game/images/character/left_idle.png"));
+            idle_right = ImageIO.read(getClass().getResourceAsStream("/game/images/character/right_idle.png"));
+
+            // Walk 1 Sprites
+            walk_up1 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/up_1.png"));
+            walk_down1 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/down_1.png"));
+            walk_left1 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/left_1.png"));
+            walk_right1 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/right_1.png"));
+
+            // Walk 2 Sprites
+            walk_up2 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/up_2.png"));
+            walk_down2 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/down_2.png"));
+            walk_left2 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/left_2.png"));
+            walk_right2 = ImageIO.read(getClass().getResourceAsStream("/game/images/character/right_2.png"));
         } catch (IOException error) {
             error.printStackTrace();
         }
