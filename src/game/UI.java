407e21a4fee8bbb1fec19c1ffa7b45a087ed6629
O <oliosborne@gmail.com>
2022-07-29 22:39:21 +0100

    Encapsulated UI class.

--- src/game/UI.java
+++ src/game/UI.java
@@ -3,56 +3,48 @@ package game;
 import java.awt.*;
 
 public class UI {
-    GamePanel gp;
-    Graphics2D g2;
-    Font fontArial20;
-    public boolean notificationActive = false;
-    public String notifictionMessage = "";
-    int notificationActiveCounter = 0;
+    private final GamePanel gp;
+
+    private Graphics2D g2;
+
+    private final Font fontArial20;
+    private final Font fontNotifications;
+
+    private boolean notificationActive = false;
+    private String notificationMessage = "";
+    private int notificationActiveCounter = 0;
 
     public UI(GamePanel gp) {
         this.gp = gp;
         fontArial20 = new Font("Arial", Font.PLAIN, 20);
+        fontNotifications = new Font("Arial", Font.BOLD, 14);
     }
 
     public void displayNotification(String text) {
-        notifictionMessage = text;
+        notificationMessage = text;
         notificationActive = true;
     }
 
     public void draw(Graphics2D g2) {
-        // Don't instantiate anything here, called in game loop.
-
         this.g2 = g2;
 
         g2.setFont(fontArial20);
         g2.setColor(Color.white);
 
-        if (gp.gameState == gp.PLAY_STATE) drawPlayScreen();
-        else if (gp.gameState == gp.PAUSE_STATE) drawPauseScreen();
+        if (gp.gameState == GamePanel.PLAY_STATE) drawPlayScreenUI();
+        else if (gp.gameState == GamePanel.PAUSE_STATE) drawPauseScreenUI();
     }
 
-    public void drawPlayScreen() {
+    private void drawPlayScreenUI() {
         g2.drawString("X: " + gp.player.getWorldX(), 25, 40);
         g2.drawString("Y: " + gp.player.getWorldY(), 25, 60);
 
-        // Notifications
         if (notificationActive) {
-            g2.setFont(g2.getFont().deriveFont(10F));
-            int x = getXForCenteredText(notifictionMessage);
-            int y = gp.screenHeight - 20;
-            g2.drawString(notifictionMessage, x, y);
-
-            notificationActiveCounter++;
-
-            if (notificationActiveCounter > 120) {
-                notificationActiveCounter = 0;
-                notificationActive = false;
-            }
+            drawNotification();
         }
     }
 
-    public void drawPauseScreen() {
+    private void drawPauseScreenUI() {
         String text = "PAUSED";
         int x = getXForCenteredText(text);
         int y = gp.screenHeight/2;
@@ -62,5 +54,21 @@ public class UI {
     private int getXForCenteredText(String text) {
         int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
         return gp.screenWidth/2 - length/2;
+    }
+
+    private void drawNotification() {
+        g2.setFont(fontNotifications);
+
+        int x = getXForCenteredText(notificationMessage);
+        int y = gp.screenHeight - 20;
+
+        g2.drawString(notificationMessage, x, y);
+
+        notificationActiveCounter++;
+
+        if (notificationActiveCounter > 120) {
+            notificationActiveCounter = 0;
+            notificationActive = false;
+        }
     }
 }
